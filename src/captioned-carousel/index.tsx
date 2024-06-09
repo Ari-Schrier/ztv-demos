adb468ad88a57d8453127164065be6fc274fd0b4
Aidan Schrier <schrier.a@northeastern.edu>
2024-06-09 18:17:16 -0400

    Let's get this online!

--- src/captioned-carousel/index.tsx
+++ src/captioned-carousel/index.tsx
@@ -2,19 +2,17 @@ import { Carousel } from 'react-bootstrap';
 
 function CaptionedCarousel(props:any){
     return (
-        <div className='container'>
             <Carousel fade>
                 {props.json.map((item:any, index: number) => (
                     <Carousel.Item key={index}>
                         <img src={`${process.env.PUBLIC_URL}/images${item.image}`} className='img-fluid' />
                         <Carousel.Caption>
-                            <h3>{item.title}</h3>
-                            <p>{item.funFact[props.language]}</p>
+                            <h1>{item.title}</h1>
+                            <h2>{item.funFact[props.language]}</h2>
                         </Carousel.Caption>
                     </Carousel.Item>
                 ))}
             </Carousel>
-        </div>
     );
 }
 
--- src/demo-server/demo-server.tsx
+++ src/demo-server/demo-server.tsx
@@ -1,9 +1,49 @@
-import { jsons } from "../database/jsons";
-function DemoServer(){
+import React, { useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import CaptionedCarousel from '../captioned-carousel';
+import { jsons } from '../database/jsons';
 
-    return(
+function DemoServer() {
+    const [activeJson, setActiveJson] = useState(jsons[2].content);
+    const [language, setActiveLanguage] = useState(0);
+
+    const handleLanguageSelect = (eventKey:any) => {
+        setActiveLanguage(Number(eventKey));
+        console.log(`Selected language: ${eventKey}`);
+    };
+    const handleSelectJson = (eventKey:any) => {
+        const selectedJson = JSON.parse(eventKey);
+        setActiveJson(selectedJson);
+        console.log(`Selected JSON:`, selectedJson);
+    };
+
+    return (
         <div className="container">
-            <h1>Hello!</h1>
+            <CaptionedCarousel json={activeJson} language={language} />
+            <div className='d-flex justify-content-center'>
+                <Dropdown onSelect={handleSelectJson}>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic" size="lg">
+                        Select Topic
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                    {jsons.map((json, index) => (
+                            <Dropdown.Item key={index} eventKey={JSON.stringify(json.content)}>
+                                {json.Title}
+                            </Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown onSelect={handleLanguageSelect}>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic" size="lg">
+                        Select Language
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item eventKey="0">English</Dropdown.Item>
+                        <Dropdown.Item eventKey="1">Spanish</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">French</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
         </div>
     );
 }
